6d5425fb8f2537d32c2559e692c8becb34290114
Carson Hartley <chartley1988@gmail.com>
2024-02-13 20:30:18 -0700

    dotfiles backup Feb 13

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,15 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+opt.textwidth = 79
+opt.wrap = true
+opt.linebreak = true
+opt.breakat = " ^I!@*-+;:,./?"
+opt.breakindent = true
+opt.colorcolumn = "79"
+
+-- Turn Spell check on
+opt.spell = true
+opt.spelllang = "en_us"
